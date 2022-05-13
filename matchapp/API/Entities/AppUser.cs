410e4e43dfaee887f97aec6ef34667ae4e4ef5cf
Ronald Figueroa <ronaldra09gtech@gmail.com>
2022-05-13 08:36:35 +0800

    securing password in database stored api

--- matchapp/API/Entities/AppUser.cs
+++ matchapp/API/Entities/AppUser.cs
@@ -10,5 +10,9 @@ namespace API.Entities
         public int id { get; set; }
 
         public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
